62e5db8eb5288e684f316d804a6bfa4aefdc4e2a
David Burrows <david.burrows@bbc.co.uk>
2020-04-03 12:29:59 +0100

    Added sample project

---
+++ GitHubActions/GitHubActions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubActions
+//
+//  Created by David Burrows on 03/04/2020.
+//  Copyright © 2020 BBC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
